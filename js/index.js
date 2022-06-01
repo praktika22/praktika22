b4bdc9fc9537ce328a1890b9ef2cbc1a6fcadb51
CatProgrammerYT <sp237752@gmail.com>
2022-06-01 23:17:45 +0300

    Added captions for regions | Made map more adaptive | Added all regions files | Wrote a function to create markers

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
 
-coords = [[], []]
+coords = [[30, 60], []]
 
-urls = []
+urls = ['#']
 
 function createMarkers() {
     var marker = document.createElement('a')
     urls.forEach(element => {
         marker.href = element
-    });
+    })
 
+    marker.style.position = 'absolute'
+    coords.forEach(i => {
+        i.forEach(q => {
+            marker.style.top = q[0]
+            marker.style.left = q[1]
+        });
+    })
 }
